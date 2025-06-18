2a63468c72e9977573b9145b86eff88d3a8bb95c
Nathan Griffiths <ngriffiths@postmedia.com>
2025-06-18 11:46:36 -0700

    initial commit

--- src/App.svelte
+++ src/App.svelte
@@ -14,7 +14,7 @@
 
     // SET CHART HEIGH & URL HERE...
     const chartHeight = '575px';
-    const flourishStoryUrl = 'https://flo.uri.sh/story/975711/embed';
+    const flourishStoryUrl = 'https://flo.uri.sh/story/3178410/embed';
 
     // FUNCTIONS
     const updateStep = function(currentStep) {
@@ -31,6 +31,12 @@
         } else if (currentStep === 2) {
             updateStep(currentStep );
         } else if (currentStep === 3) {
+            updateStep(currentStep);
+        } else if (currentStep === 4) {
+            updateStep(currentStep);
+        } else if (currentStep === 5) {
+            updateStep(currentStep);
+        } else if (currentStep === 6) {
             updateStep(currentStep);
         }
     };
@@ -57,10 +63,7 @@
 </ul>
 
 <footer>
-    <p class="source">Source:
-        <a href="https://www.vancouversun.com/" target="_blank">tk</a>,
-        <a href="https://www.openstreetmap.org/" target="_blank">tk</a>
-    </p>
+    <p class="source"></p>
     <!-- flourish logo -->
     <div style='width:100%!;margin-top:4px!important;text-align:right!important;'><a class='flourish-credit' href='https://public.flourish.studio/visualisation/3706064/?utm_source=embed&utm_campaign=visualisation/3706064' target='_top' style='text-decoration:none!important'><img alt='Made with Flourish' src='https://public.flourish.studio/resources/made_with_flourish.svg' style='width:105px!important;height:16px!important;border:none!important;margin:0!important;'> </a></div>
 </footer>
